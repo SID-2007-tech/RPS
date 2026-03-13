c26340dc769ee5c6e96b6971bf2d780a89046db2
SID-2007-tech <sp8k2007@gmail.com>
2026-03-13 23:55:12 +0530

    function getHumanChoice defined

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function getComputerChoice() {
     return "scissor";
   }
 }
+
+function getHumanChoice(){
+    let msg = "Please choose one of the following\n1. rock\n2. paper\n3. scissor";
+    let humanChoice = prompt(msg);
+    return humanChoice;
+}
+console.log(getHumanChoice());
