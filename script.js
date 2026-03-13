def2beafa08ca4a2a65b7eebacea38e85c82727e
SID-2007-tech <sp8k2007@gmail.com>
2026-03-14 00:15:29 +0530

    score variables and playRound function defined

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let x = Math.random() * 3;
   if (x >= 2) {
     return "rock";
-  } 
-  else if (x >= 1) {
+  } else if (x >= 1) {
     return "paper";
-  } 
-  else {
-    return "scissor";
+  } else {
+    return "scissors";
   }
 }
 
-function getHumanChoice(){
-    let msg = "Please choose one of the following\n1. rock\n2. paper\n3. scissor";
-    let humanChoice = prompt(msg);
-    return humanChoice;
+function getHumanChoice() {
+  let msg =
+    "Please choose one of the following\n1. rock\n2. paper\n3. scissors";
+  let humanChoice = prompt(msg);
+  return humanChoice;
 }
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+  if (humanChoice == "ROCK" && computerChoice == "SCISSORS"){
+    humanScore++;
+    return ("You won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (computerChoice == "ROCK" && humanChoice == "SCISSORS"){
+    computerScore++;
+    return("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+  if (humanChoice == "ROCK" && computerChoice == "PAPER"){
+    humanScore++;
+    return("You won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (computerChoice == "ROCK" && humanChoice == "PAPER"){
+    computerScore++;
+    return("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+  if (humanChoice == "SCISSORS" && computerChoice == "PAPER"){
+    humanScore++;
+    return("You won! " + humanChoice + " beats " + computerChoice);
+  }
+  if (computerChoice == "SCISSORS" && humanChoice == "PAPER"){
+    computerScore++;
+    return("You lose! " + computerChoice + " beats " + humanChoice);
+  }
+ 
+}
+
